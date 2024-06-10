77584040673e48738aa87d316e1d3edf4dbe715f
xiangzezhen01 <1628996791@qq.com>
2024-06-10 13:41:55 +0800

    train data label, data smo, RFC train

--- DaL_Regressor.py
+++ DaL_Regressor.py
@@ -5,11 +5,10 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.feature_selection import mutual_info_regression
 from sklearn.tree import DecisionTreeRegressor
-
+from imblearn.over_sampling import SMOTE
 import configs
 from utils.general import build_model, recursive_dividing
 from numpy import recfromcsv
-
 
 # todo:  2. clustering_indexs and clustering_index_all 3. total index and training index(no zero)  4. if N_train to big?
 
@@ -22,7 +21,6 @@ def get_whole_data(path):
 class DaL_Regressor:
 
     def __init__(self, data_path):
-        self.RFC = None
         self.whole_data = None
         self.other_env_data = None
         self.data_path = data_path
@@ -43,9 +41,13 @@ class DaL_Regressor:
             weight = feature_weights[i]
             self.weights.append(weight)
         self.train_set = self.whole_data[self.train_index]
-        self.clusters = self.get_cluster(self.whole_data[self.train_index])  # todo: right？
-        self.train_RFC()
-        pass
+        self.clusters = self.get_cluster(self.whole_data[self.train_index])
+        self.clusters_label = list()
+        self.generate_cluster_label()
+        self.x_smo = None
+        self.y_smo = None
+        self.RFC = RandomForestClassifier(criterion='gini', random_state=3).fit(self.x_smo, self.y_smo)
+
 
     # todo: need to test the function
     def get_cluster(self, whole_train_data):
@@ -63,16 +65,22 @@ class DaL_Regressor:
     # build a random forest classifier to classify testing samples
 
     def generate_cluster_label(self):
-
+        clusters_list = list()
+        for i in range(len(self.clusters)):
+            clusters_list.append(np.full(len(self.clusters[i]), i))
+        self.clusters_label = np.concatenate(clusters_list)
         return None
 
     def data_smo(self):
-        x_som = self.whole_data[self.train_index, 0:self.feather_count]
-        y_som = None
+        x_smo = self.whole_data[self.train_index, 0:self.feather_count]
+        for j in range(self.feather_count):
+            x_smo[:, j] = x_smo[:, j] * self.weights[j]
+        y_smo = self.clusters_label
+        smo = SMOTE(random_state=1, k_neighbors=3)
+        self.x_smo, self.y_smo = smo.fit_resample(x_smo, y_smo)
 
-    def train_RFC(self):
-        self.RFC = RandomForestClassifier(criterion='gini', random_state=0)
-        pass
+
+
 
 
 dal = DaL_Regressor("data/Apache_AllNumeric.csv")
